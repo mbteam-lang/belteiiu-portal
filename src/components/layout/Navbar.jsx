65dec051a9dc5447b65606bcc6f4ef5e7c837169
sokliNnuon1 <soklin061@gmail.com>
2026-08-19 09:53:04 +0700

    Update about problem with colum

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -136,7 +136,7 @@ const Navbar = () => {
                             openMenuIndex={openMenuIndex}
                             toggleMenuIndex={toggleMenuIndex}
                             t={t}
-                            // to="/list_main"
+                            to="/list_main"
                         />
                         <DesktopDropdown
                             index="dorm"
